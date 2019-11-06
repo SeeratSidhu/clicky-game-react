48cb1e7b1a5c3edce0b76baf3debc218fe4767fb
Manseerat Kaur <seeratsidhu01@gmail.com>
2019-11-06 15:16:23 -0500

    Updates

---
+++ precache-manifest.0131b221471f3140b24fff4cac433a17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80fdb7716ec34d12d36ea54b805a130f",
+    "url": "/clicky-game-react/index.html"
+  },
+  {
+    "revision": "d1b80b8d0ff52332de33",
+    "url": "/clicky-game-react/static/css/main.3f6b8fd2.chunk.css"
+  },
+  {
+    "revision": "49ef6d7d0827c0599ecb",
+    "url": "/clicky-game-react/static/js/2.98690820.chunk.js"
+  },
+  {
+    "revision": "d1b80b8d0ff52332de33",
+    "url": "/clicky-game-react/static/js/main.755666e0.chunk.js"
+  },
+  {
+    "revision": "0487a411ee2c9f8f3498",
+    "url": "/clicky-game-react/static/js/runtime-main.dd624c6e.js"
+  }
+]);
